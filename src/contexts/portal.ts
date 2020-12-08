418722da50e3148d6148ad6e5c942ddd8f687ba6
Mo Gorhom <gorhom@me.com>
2020-12-08 22:26:29 +0100

    fix: typing for usePortal

--- src/contexts/portal.ts
+++ src/contexts/portal.ts
@@ -1,9 +1,7 @@
 import { createContext } from 'react';
 import type { PortalMethods } from '../types';
 
-interface PortalContextType extends PortalMethods {}
-
-export const PortalContext = createContext<PortalContextType>({
+export const PortalContext = createContext<PortalMethods>({
   mount: () => {},
   unmount: () => {},
 });
