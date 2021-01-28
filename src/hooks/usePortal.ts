ed1355047801e3bb9eb6bad4fed279be9fcaa171
Mo Gorhom <gorhom@me.com>
2021-01-28 22:03:36 +0100

    chore: added no context provided error on usePortal

--- src/hooks/usePortal.ts
+++ src/hooks/usePortal.ts
@@ -1,4 +1,14 @@
 import { useContext } from 'react';
 import { PortalContext } from '../contexts';
 
-export const usePortal = () => useContext(PortalContext);
+export const usePortal = () => {
+  const value = useContext(PortalContext);
+
+  if (value === null) {
+    throw new Error(
+      "Portal context cannot be null, please add 'PortalHost' to the root component."
+    );
+  }
+
+  return value;
+};
