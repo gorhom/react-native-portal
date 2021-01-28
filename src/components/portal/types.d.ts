30bdf1c2d9968f1489ee83633066f49d4ac7b85c
Mo Gorhom <gorhom@me.com>
2021-01-28 22:04:59 +0100

    chore: added mounting handlers

--- src/components/portal/types.d.ts
+++ src/components/portal/types.d.ts
@@ -8,6 +8,20 @@ export interface PortalProps {
    */
   name?: string;
   /**
+   * Override internal mounting functionality, this is useful
+   * if you want to trigger any action before mounting the portal content.
+   * @type (mount?: () => void) => void
+   * @default undefined
+   */
+  handleOnMount?: (mount?: () => void) => void;
+  /**
+   * Override internal un-mounting functionality, this is useful
+   * if you want to trigger any action before un-mounting the portal content.
+   * @type (mount?: () => void) => void
+   * @default undefined
+   */
+  handleOnUnmount?: (unmount?: () => void) => void;
+  /**
    * Portal content.
    */
   children?: ReactNode | ReactNode[];
